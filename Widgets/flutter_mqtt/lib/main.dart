c3331fc6a0caf3486c856cede7f3e204e4cac5b6
Vinithkumar-S <vk3228433@gmail.com>
2024-12-09 11:57:24 +0530

    add

---
+++ Widgets/flutter_mqtt/lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      //home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MqttWidget extends StatefulWidget {
+  const MqttWidget({super.key});
+
+  @override
+  State<MqttWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MqttWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("MQTT"),
+      ),
+    );
+  }
+}
